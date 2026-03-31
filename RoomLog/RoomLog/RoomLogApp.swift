d6dee67525ed414099caee01969318cf19efb431
Doyeon Kim <ehdus022191@dgu.ac.kr>
2026-03-31 17:15:18 +0900

    Initial commit

---
+++ RoomLog/RoomLog/RoomLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoomLogApp.swift
+//  RoomLog
+//
+//  Created by 김도연 on 3/31/26.
+//
+
+import SwiftUI
+
+@main
+struct RoomLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
