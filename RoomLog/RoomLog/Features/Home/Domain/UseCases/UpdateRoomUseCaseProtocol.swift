96365c115e25e8979c1c983cbfeb5c2e326910b8
Doyeon Kim <ehdus022191@dgu.ac.kr>
2026-04-07 03:28:31 +0900

    feat: 홈 화면 및 방 관리 UseCaseProtocol 정의 ( #7 )

---
+++ RoomLog/RoomLog/Features/Home/Domain/UseCases/UpdateRoomUseCaseProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  UpdateRoomUseCaseProtocol.swift
+//  RoomLog
+//
+//  Created by 김도연 on 4/7/26.
+//
+
+import Foundation
+
+/// 방 상세 정보 업데이트 UseCaseProtocol
+protocol UpdateRoomUseCaseProtocol {
+    func execute(roomId: Int) async throws -> UpdatedRoomDetail
+}
