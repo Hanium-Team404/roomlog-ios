f852d050b4dd615219f0a7c137f78b6ceba34eb5
minkyo <smk78449@naver.com>
2026-04-12 23:39:21 +0900

    feat: Defect(하자 점검)의 UseCase정의

---
+++ RoomLog/RoomLog/Features/Defect/Domain/UseCases/GetDefectRoomDataUseCaseProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  GetDefectRoomDataUseCase.swift
+//  
+//
+//  Created by 송민교 on 4/12/26.
+//
+
+import Foundation
+
+/// 맨 처음 방 조회 UseCaseProtocol
+protocol GetDefectRoomDataUseCaseProtocol {
+    func execute() async throws -> DefectRoomData
+}
