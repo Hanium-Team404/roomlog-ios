2508d5f90b370a743b7abb7fb80cbdb4bf1d4818
Doyeon Kim <ehdus022191@dgu.ac.kr>
2026-04-14 00:44:18 +0900

    add: DefectUseCaseProvider 추가

---
+++ RoomLog/RoomLog/Features/Defect/Presentation/Provider/DefectUseCaseProvider.swift
@@ -0,0 +1,37 @@
+//
+//  DefectUseCaseProvider.swift
+//  RoomLog
+//
+//  Created by 김도연 on 4/14/26.
+//
+
+import Foundation
+
+protocol DefectUseCaseProvider {
+    func makeGetDefectRoomDataUseCase() -> GetDefectRoomDataUseCaseProtocol
+    func makeGetDefectReportUseCase() -> GetDefectReportUseCaseProtocol
+    func makeGetDefectReportDetailUseCase() -> GetDefectReportDetailUseCaseProtocol
+}
+
+final class DefectUseCaseProviderImpl: DefectUseCaseProvider {
+    // MARK: - Repository
+    private let defectRepository: DefectRepositoryProtocol
+
+    // MARK: - Init
+    init(adapter: MoyaNetworkAdapter) {
+        self.defectRepository = DefectRepository(adapter: adapter)
+    }
+
+    // MARK: - Defect UseCases
+    func makeGetDefectRoomDataUseCase() -> GetDefectRoomDataUseCaseProtocol {
+        GetDefectRoomDataUseCase(repository: defectRepository)
+    }
+
+    func makeGetDefectReportUseCase() -> GetDefectReportUseCaseProtocol {
+        GetDefectReportUseCase(repository: defectRepository)
+    }
+
+    func makeGetDefectReportDetailUseCase() -> GetDefectReportDetailUseCaseProtocol {
+        GetDefectReportDetailUseCase(repository: defectRepository)
+    }
+}
