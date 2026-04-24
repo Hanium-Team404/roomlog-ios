a80548137bbcf74ef3b9c69868d8efffef710d6d
minkyo <smk78449@naver.com>
2026-04-24 14:20:54 +0900

    feat: Domain layer의 interface 정의

--- RoomLog/RoomLog/Features/Viewer/Defect/Domain/Interfaces/DefectRepositoryProtocol.swift
+++ RoomLog/RoomLog/Features/Viewer/Defect/Domain/Interfaces/DefectRepositoryProtocol.swift
@@ -8,7 +8,7 @@ import Foundation
 
 protocol DefectRepositoryProtocol {
     /// 메인 하자 점검 방 조회
-    func getDefectRoomData() async throws -> DefectRoomData
+    func getDefectRoomData() async throws -> [DefectRoomData]
     
     /// 선택한 방 하자 정보 조회
     func getDefectReport(roomId: Int) async throws -> DefectReport
