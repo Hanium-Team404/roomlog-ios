5472f7ca07c8bd1991c6e99158aee7055afc98b3
minkyo <smk78449@naver.com>
2026-04-24 14:18:25 +0900

    feat: Defect(하자 점검)의 view 구현

--- RoomLog/RoomLog/Features/Viewer/Presentation/ViewerView.swift
+++ RoomLog/RoomLog/Features/Viewer/Presentation/ViewerView.swift
@@ -7,7 +7,53 @@
 import SwiftUI
 
 struct ViewerView: View {
+    @Environment(\.di) var di
     var body: some View {
+        let pathStore = di.resolve(PathStore.self)
         
+        List {
+            // 하자 점검
+            Button {
+                pathStore.defectPath.append(.defect(.defectList))
+            } label: {
+                VStack {
+                    HStack {
+                        VStack(alignment: .leading, spacing: 4) {
+                            Text("하자 점검")
+                                .font(.system(.title2))
+                                .foregroundStyle(Color.black)
+                            Text("내 방의 하자 정보 점검하기")
+                                .foregroundStyle(Color.gray)
+                        }
+                        Spacer()
+                        Image(systemName: "chevron.right")
+                            .foregroundStyle(Color.gray)
+                    }
+                }
+                .padding(.vertical, 8)
+            }
+            
+            // 내 방 비교하기
+            Button {
+                // TODO: 페이지 연결
+            } label: {
+                VStack {
+                    HStack {
+                        VStack(alignment: .leading, spacing: 4) {
+                            Text("내 방 비교")
+                                .font(.system(.title2))
+                                .foregroundStyle(Color.black)
+                            Text("입주 전/후 내 방 비교하기")
+                                .foregroundStyle(Color.gray)
+                        }
+                        Spacer()
+                        Image(systemName: "chevron.right")
+                            .foregroundStyle(Color.gray)
+                    }
+                }
+                .padding(.vertical, 8)
+            }
+        }
+        .navigationTitle("RoomLog")
     }
 }
