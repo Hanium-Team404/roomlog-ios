93c66db4289254cc6ef848656045025938d72061
Doyeon Kim <ehdus022191@dgu.ac.kr>
2026-05-05 19:40:10 +0900

    refactor: Domain Models 폴더 구조 정리
    Home/ 하위 폴더를 제거하고 Models/ 직속으로 이동

---
+++ RoomLog/RoomLog/Features/Home/Domain/Models/House.swift
@@ -0,0 +1,15 @@
+//
+//  House.swift
+//  RoomLog
+//
+//  Created by 김도연 on 5/4/26.
+//
+
+import Foundation
+
+struct House: Codable {
+    let houseId: Int
+    let name: String
+    let x: Float
+    let y: Float
+}
