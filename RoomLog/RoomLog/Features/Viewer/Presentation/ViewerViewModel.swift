e278bab7a3482076194437a0570f29116061fbb9
minkyo <smk78449@naver.com>
2026-05-07 18:18:55 +0900

    feat: viewer UI 구현

---
+++ RoomLog/RoomLog/Features/Viewer/Presentation/ViewerViewModel.swift
@@ -0,0 +1,28 @@
+//
+//  ViewerViewModel.swift
+//  RoomLog
+//
+//  Created by 송민교 on 5/6/26.
+//
+
+import Foundation
+
+@Observable
+final class ViewerViewModel {
+    private(set) var rooms: [DefectRoomData] = []
+    private(set) var isLoading = false
+
+    private let useCase: GetDefectRoomDataUseCaseProtocol
+
+    init(useCase: GetDefectRoomDataUseCaseProtocol) {
+        self.useCase = useCase
+    }
+
+    func fetchRooms() async {
+        isLoading = true
+        defer { isLoading = false }
+        do {
+            rooms = try await useCase.execute()
+        } catch {}
+    }
+}
