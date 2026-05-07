9679d8583bb3633c790e1c1b0b3098fbb14032d8
minkyo <smk78449@naver.com>
2026-05-07 18:24:17 +0900

    feat: UseCase 추가 및 UseCase implementation

--- RoomLog/RoomLog/Features/Defect/Domain/UseCases/GetDefectReportDetailUseCaseProtocol.swift
+++ RoomLog/RoomLog/Features/Defect/Domain/UseCases/GetDefectReportDetailUseCaseProtocol.swift
@@ -9,5 +9,5 @@ import Foundation
 
 /// 선택한 방의 하자 상세 정보 조회 UseCaseProtocol
 protocol GetDefectReportDetailUseCaseProtocol {
-    func execute(roomId: Int, reportId: Int) async throws -> DefectReportDetail
+    func execute(roomId: Int) async throws -> DefectReportDetail
 }
--- RoomLog/RoomLog/Features/Defect/Domain/UseCases/Implementations/GetDefectRoomDataUseCase.swift
+++ RoomLog/RoomLog/Features/Defect/Domain/UseCases/Implementations/GetDefectRoomDataUseCase.swift
@@ -8,15 +8,12 @@
 import Foundation
 
 final class GetDefectRoomDataUseCase: GetDefectRoomDataUseCaseProtocol {
-    // MARK: - Property
     private let repository: DefectRepositoryProtocol
 
-    // MARK: - Init
     init(repository: DefectRepositoryProtocol) {
         self.repository = repository
     }
 
-    // MARK: - Function
     func execute() async throws -> [DefectRoomData] {
         try await repository.getDefectRoomData()
     }
