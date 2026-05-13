240628f231bc61d85bc06413d83dcf0a4b4e3a18
Doyeon Kim <ehdus022191@dgu.ac.kr>
2026-05-13 18:32:02 +0900

    feat: RoomDetailView 및 PLYSceneView 구현
    - RoomDetailView / RoomDetailViewModel 구현 (방 상세 + PLY 3D 뷰어)
    - PLYSceneView 공용 컴포넌트 추출 (SceneKit + ModelIO)
    - NavigationDestination에 roomDetail, plyPreview 케이스 추가
    - NavigationRoutingView에 ScanView 새 init 반영

---
+++ RoomLog/RoomLog/Features/Home/Presentation/Components/PLYSceneView.swift
@@ -0,0 +1,36 @@
+//
+//  PLYSceneView.swift
+//  RoomLog
+//
+//  Created by 김도연 on 5/12/26.
+//
+
+import SwiftUI
+import SceneKit
+import ModelIO
+import SceneKit.ModelIO
+
+struct PLYSceneView: UIViewRepresentable {
+    let fileURL: URL
+
+    func makeUIView(context: Context) -> SCNView {
+        let scnView = SCNView()
+        scnView.backgroundColor = .systemBackground
+        scnView.allowsCameraControl = true
+        scnView.autoenablesDefaultLighting = true
+
+        if let scene = loadScene(from: fileURL) {
+            scnView.scene = scene
+        }
+
+        return scnView
+    }
+
+    func updateUIView(_ uiView: SCNView, context: Context) {}
+
+    private func loadScene(from url: URL) -> SCNScene? {
+        let asset = MDLAsset(url: url)
+        asset.loadTextures()
+        return SCNScene(mdlAsset: asset)
+    }
+}
