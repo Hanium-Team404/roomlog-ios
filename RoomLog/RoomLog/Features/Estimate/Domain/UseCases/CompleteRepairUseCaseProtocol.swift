b6be2dded6c2f60e323a73df61fc1f41849d5751
minkyo <smk78449@naver.com>
2026-05-17 19:05:44 +0900

    feat: 수리내역조회 UseCase 및 UseCase 구현

---
+++ RoomLog/RoomLog/Features/Estimate/Domain/UseCases/CompleteRepairUseCaseProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  CompleteRepairUseCaseProtocol.swift
+//  RoomLog
+//
+//  Created by 송민교 on 5/17/26.
+//
+
+import Foundation
+
+protocol CompleteRepairUseCaseProtocol {
+    func execute(estimateId: Int) async throws
+}
